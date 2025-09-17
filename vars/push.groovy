7d5e5a192097307725fdb817a6cf0699f8e5734c
Aditya Ghatiya <91601167+Adityaghatiya@users.noreply.github.com>
2025-09-17 22:21:23 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,14 +1,8 @@
 def call(String Project, String ImageTag, String dockerhubuser) {
-    withCredentials([usernamePassword(credentialsId: 'dockercreds',
-                                      usernameVariable: 'USERNAME',
-                                      passwordVariable: 'PASSWORD')]) {
-        // Tag the image before pushing
-        sh "docker tag ${Project}:${ImageTag} ${USERNAME}/${Project}:${ImageTag}"
-        
-        // Secure login using --password-stdin
-        sh "echo ${PASSWORD} | docker login -u ${USERNAME} --password-stdin"
-        
-        // Push to DockerHub
-        sh "docker push ${USERNAME}/${Project}:${ImageTag}"
-    }
+    echo "Building Docker image: ${Project}:${ImageTag}"
+    
+    // Always build with local name first
+    sh "docker build -t ${Project}:${ImageTag} ."
+    
+    echo "Docker image ${Project}:${ImageTag} built successfully"
 }
