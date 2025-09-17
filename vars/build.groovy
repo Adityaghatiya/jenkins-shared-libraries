6a7dd21fdc12811f98227c9588a506140ba6287f
Aditya Ghatiya <91601167+Adityaghatiya@users.noreply.github.com>
2025-09-17 21:23:42 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,5 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser){
+  echo 'Building Docker image'
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+  echo 'docker image created'
 }
